d52b82290c1f61f6a0ff1ffea9a83e6d8a8980f7
Putra <puterayasa180@gmail.com>
2025-03-26 17:35:19 +0800

    bagian login page

---
+++ src/loginPage/Welcome.java
@@ -0,0 +1,19 @@
+package loginPage;
+
+public class Welcome {
+    public static void displayWelcome (){
+        System.out.println("|===============================================|");
+        System.out.println("|                                               |");
+        System.out.println("|      SELAMAT DATANG DI PROGRAM INVESTASI      |");
+        System.out.println("|                                               |");
+        System.out.println("|===============================================|");
+    }
+
+    public static void displayLogin(){
+        System.out.println("|===============================================|");
+        System.out.println("|                                               |");
+        System.out.println("|         SILAHKAN LENGKAPI BAGIAN INI!         |");
+        System.out.println("|                                               |");
+        System.out.println("|===============================================|");
+    }
+}
