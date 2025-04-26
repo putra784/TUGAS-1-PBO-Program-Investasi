d9081573c14a621f2cf69327a30d07435d5f2d13
Putra <puterayasa180@gmail.com>
2025-04-26 14:03:32 +0800

    autentikasi login user

--- src/loginPage/AutentikasiUser.java
+++ src/loginPage/AutentikasiUser.java
@@ -1,9 +1,13 @@
 package loginPage;
 
+import menu.MenuHandler;
 import java.util.Scanner;
 
-public class AuthenticationUser {
-    public static void authenticationUser (){
+public class AutentikasiUser {
+
+    static MenuHandler menuHandler = new MenuHandler();
+
+    public static void autentikasiUser (){
         Scanner scanner = new Scanner (System.in);
 
         System.out.print("Masukkan role / peran anda (hanya admin atau customer): ");
@@ -24,16 +28,19 @@ public class AuthenticationUser {
         System.out.print("Masukkan password anda: ");
         String password = scanner.nextLine();
 
-        checkUser(role, username, password);
+        cekUser(role, username, password, roleInput);
 
     }
 
-    private static void checkUser (Role role, String username, String password){
+    private static void cekUser (Role role, String username, String password, String roleInput){
         User user = hardcodedUser.findUser(username);
 
         if (user == null) System.out.println("Username tidak ditemukan!");
         else if (user.getRole() != role) System.out.println("Peran tidak sesuai!");
         else if (!user.getPassword().equals(password)) System.out.println("Password salah!");
-        else ;
+        else {
+            if (roleInput.equals("CUSTOMER")) menuHandler.displayCustomerMenu();
+            else menuHandler.displayAdminMenu();
+        }
     }
 }
