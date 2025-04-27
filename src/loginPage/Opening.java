80519564751eab5e8521054049bbdf5de9adff27
Putra <puterayasa180@gmail.com>
2025-04-27 21:55:09 +0800

    tampilan awal

--- src/loginPage/Opening.java
+++ src/loginPage/Opening.java
@@ -1,17 +1,19 @@
 package loginPage;
 
-public class Welcome {
+public class Opening {
     public static void displayWelcome (){
         System.out.println("|===============================================|");
         System.out.println("|                                               |");
         System.out.println("|      SELAMAT DATANG DI PROGRAM INVESTASI      |");
         System.out.println("|                                               |");
         System.out.println("|===============================================|");
+        System.out.println();
     }
 
     public static void displayLogin(){
         System.out.println("|===============================================|");
         System.out.println("|         SILAHKAN LENGKAPI BAGIAN INI!         |");
         System.out.println("|===============================================|");
+        System.out.println();
     }
 }
