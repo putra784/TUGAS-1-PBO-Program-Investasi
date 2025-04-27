5b7a3e079e6c677c0c3ebd7b6a59963716a55b9c
Putra <puterayasa180@gmail.com>
2025-04-27 21:47:35 +0800

    update autentikasi login

--- src/loginPage/AutentikasiUser.java
+++ src/loginPage/AutentikasiUser.java
@@ -1,44 +1,42 @@
 package loginPage;
 
 import menu.MenuHandler;
-import java.util.Scanner;
+import utility.InputUser; // <- Import utility InputUser
 
 public class AutentikasiUser {
 
-    public static void autentikasiUser (){
-        Scanner scanner = new Scanner (System.in);
-
-        System.out.print("Masukkan role / peran anda (hanya admin atau customer): ");
-        String roleInput = scanner.nextLine().toUpperCase();
+    public static void autentikasiUser() {
+        String roleInput = InputUser.nextLine("Masukkan role / peran anda (hanya admin atau customer): ").toUpperCase();
 
         Role role;
         try {
             role = Role.valueOf(roleInput);
-        }
-        catch (IllegalArgumentException e) {
+        } catch (IllegalArgumentException e) {
             System.out.println("Peran tidak ditemukan!");
             return;
         }
 
-        System.out.print("Masukkan username anda: ");
-        String username = scanner.nextLine();
-
-        System.out.print("Masukkan password anda: ");
-        String password = scanner.nextLine();
+        String username = InputUser.nextLine("Masukkan username anda: ");
+        String password = InputUser.nextLine("Masukkan password anda: ");
 
         cekUser(role, username, password, roleInput);
-
     }
 
-    private static void cekUser (Role role, String username, String password, String roleInput){
+    private static void cekUser(Role role, String username, String password, String roleInput) {
         User user = hardcodedUser.findUser(username);
 
-        if (user == null) System.out.println("Username tidak ditemukan!");
-        else if (user.getRole() != role) System.out.println("Peran tidak sesuai!");
-        else if (!user.getPassword().equals(password)) System.out.println("Password salah!");
-        else {
-            if (roleInput.equals("CUSTOMER")) MenuHandler.displayCustomerMenu();
-            else MenuHandler.displayAdminMenu();
+        if (user == null) {
+            System.out.println("Username tidak ditemukan!");
+        } else if (user.getRole() != role) {
+            System.out.println("Peran tidak sesuai!");
+        } else if (!user.getPassword().equals(password)) {
+            System.out.println("Password salah!");
+        } else {
+            if (roleInput.equals("CUSTOMER")) {
+                MenuHandler.displayCustomerMenu();
+            } else {
+                MenuHandler.displayAdminMenu();
+            }
         }
     }
 }
