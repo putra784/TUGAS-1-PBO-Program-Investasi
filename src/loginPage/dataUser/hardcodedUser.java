837b7073ad6966d2e1699ade12cf82dd4912c353
Aryawikananda <aryawikananda45@gmail.com>
2025-04-28 14:27:50 +0800

    update hardcodedUser

--- src/loginPage/dataUser/hardcodedUser.java
+++ src/loginPage/dataUser/hardcodedUser.java
@@ -1,17 +1,20 @@
-package loginPage;
+package loginPage.dataUser;
 
 import java.util.HashMap;
 
 public class hardcodedUser {
+
     private static final HashMap <String, User> userData = new HashMap<>();
 
+    // Hardcode user
     static {
         userData.put("Putera", new User("Puterayasa", "putera123", Role.ADMIN));
         userData.put("Arya", new User ("Aryawikananda", "arya321", Role.CUSTOMER));
         userData.put("Eri", new User ("Eriwidura", "eri333", Role.CUSTOMER));
     }
 
-    public static User findUser(String usersname) {
+    // method untuk menemukan user dari username
+    public static User temukanUser(String usersname) {
         for (User user : userData.values()) {
             if (user.getUsername().equals(usersname)) {
                 return user;
@@ -19,4 +22,5 @@ public class hardcodedUser {
         }
         return null;
     }
+
 }
