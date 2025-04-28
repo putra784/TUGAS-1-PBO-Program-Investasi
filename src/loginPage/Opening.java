c74f05a7a9efbeb12ac715498474ce131c1f16bd
Putra <puterayasa180@gmail.com>
2025-04-28 20:21:25 +0800

    update tampilan opening

--- src/loginPage/Opening.java
+++ src/loginPage/Opening.java
@@ -1,19 +1,19 @@
 package loginPage;
 
+// Class untuk menampilkan sambutan
 public class Opening {
     public static void displayWelcome (){
-        System.out.println("|===============================================|");
-        System.out.println("|                                               |");
-        System.out.println("|      SELAMAT DATANG DI PROGRAM INVESTASI      |");
-        System.out.println("|                                               |");
-        System.out.println("|===============================================|");
+        System.out.println("===================================================");
+        System.out.println("||                                               ||");
+        System.out.println("||      SELAMAT DATANG DI PROGRAM INVESTASI      ||");
+        System.out.println("||                                               ||");
+        System.out.println("===================================================");
         System.out.println();
     }
 
     public static void displayLogin(){
-        System.out.println("|===============================================|");
-        System.out.println("|         SILAHKAN LENGKAPI BAGIAN INI!         |");
-        System.out.println("|===============================================|");
-        System.out.println();
+        System.out.println("===================================================");
+        System.out.println("||         SILAHKAN LENGKAPI BAGIAN INI!         ||");
+        System.out.println("---------------------------------------------------");
     }
 }
