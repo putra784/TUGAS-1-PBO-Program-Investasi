f5bd200c0d2fe5662a09f87f80a84885733a0281
Putra <puterayasa180@gmail.com>
2025-04-28 23:22:19 +0800

    Update Portofolio sbn customer

--- src/menu/customer/portofolio/PortofolioSbnCustomer.java
+++ src/menu/customer/portofolio/PortofolioSbnCustomer.java
@@ -1,19 +1,20 @@
 package menu.customer.portofolio;
 
 import menu.sbnLists.SBN;
-
 import java.text.DecimalFormat;
 
-// Class untuk menyimpan data portofolio SBN Customer
+// Class untuk menyimpan portofolio SBN customer
 public class PortofolioSbnCustomer {
     private final SBN sbn;
-    private final int jumlahUnit;
-    private final double kupon;
+    private int jumlahUnit;
+    private double kupon;
+    private double totalInvestasi;
 
-    public PortofolioSbnCustomer(SBN sbn, int jumlahUnit) {
+    public PortofolioSbnCustomer(SBN sbn, double nominalInvestasi) {
         this.sbn = sbn;
-        this.jumlahUnit = jumlahUnit;
-        this.kupon = (sbn.getBunga() / 12) * 0.90 * sbn.getHargaPerUnit() * jumlahUnit;
+        this.jumlahUnit = (int) (nominalInvestasi / sbn.getHargaPerUnit());
+        this.totalInvestasi = this.jumlahUnit * sbn.getHargaPerUnit();
+        this.kupon = (sbn.getBunga() / 100) / 12 * 0.90 * totalInvestasi;
     }
 
     public void displayPortofolio() {
@@ -21,7 +22,20 @@ public class PortofolioSbnCustomer {
         System.out.println("Kode SBN: " + sbn.getKodeSbn());
         System.out.println("Jenis SBN: " + sbn.getJenisSbn());
         System.out.println("Jumlah Unit: " + jumlahUnit);
-        System.out.println("Total Bunga Per Bulan: Rp" + df.format(kupon));
+        System.out.println("Total Investasi: Rp" + df.format(totalInvestasi));
+        System.out.println("Estimasi Kupon per Bulan: Rp" + df.format(kupon));
         System.out.println("------------------------------------");
+    }
+
+    // Menambah unit dan memperbarui total investasi serta kupon
+    public void tambahUnit(double nominalInvestasi, SBN sbn) {
+        int tambahanUnit = (int) (nominalInvestasi / sbn.getHargaPerUnit());
+        this.jumlahUnit += tambahanUnit;
+        this.totalInvestasi += tambahanUnit * sbn.getHargaPerUnit();
+        this.kupon = (sbn.getBunga() / 100) / 12 * 0.90 * totalInvestasi;
+    }
+
+    public SBN getSbn() {
+        return sbn;
     }
 }
